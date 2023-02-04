c07fd856b49b8df93f97125741929c163ea9b5ba
carlosmaresdev <microsystemags@hotmail.com>
2023-02-03 21:20:31 -0600

    Add First Project

---
+++ Hola Mundo/Hola Mundo/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Hola Mundo
+//
+//  Created by carlosmaresdev on 03/02/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("Hola Mundo")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Hola Mundo/Hola Mundo/Hola_MundoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hola_MundoApp.swift
+//  Hola Mundo
+//
+//  Created by carlosmaresdev on 03/02/23.
+//
+
+import SwiftUI
+
+@main
+struct Hola_MundoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
