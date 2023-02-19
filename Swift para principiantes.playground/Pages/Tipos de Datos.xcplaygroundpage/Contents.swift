5ffa0da51d4aad440e4afe2588fe0071cd00e70e
carlosmaresdev <microsystemags@hotmail.com>
2023-02-19 01:07:44 -0600

    Se Ven los Temas de Variables y Constantes al igual que los tipos de datos

---
+++ Swift para principiantes.playground/Pages/Tipos de Datos.xcplaygroundpage/Contents.swift
@@ -0,0 +1,32 @@
+import UIkit
+
+//String
+
+let myString: String = "Hola Hackerman!"
+let myString2 = "Bienbenidos a MoudeDev"
+let myString3 = myString + " " + myString2
+print(myString3)
+
+//Inr
+
+let myInt: Int = 1
+let myInt2 = 2
+print(myInt + myInt2)
+
+//Double
+
+let myDouble: Double = 1.5
+let myDouble2 = 2.5
+print(myDouble + myDouble2)
+
+//Float
+let myFloat: Float = 1.5
+let myFloat2: Float = 2.6
+print(myFloat + myFloat2)
+
+//Bool
+let myBool = true
+let myBool2 = false
+//print(myBool + myBool2)
+print(myBool && myBool2)
+print(myBool == myBool2)
--- Swift para principiantes.playground/Pages/Variables y Constantes.xcplaygroundpage/Contents.swift
+++ Swift para principiantes.playground/Pages/Variables y Constantes.xcplaygroundpage/Contents.swift
@@ -1,3 +1,40 @@
 import UIKit
 
+//Variables
 
+var myFirstVariable = "Hellow Hackerman!"
+var myFirstNumber = 1
+
+print(myFirstVariable)
+
+myFirstVariable = "Bienvenidos a MoureDev"
+
+print(myFirstVariable)
+
+//No podemos asignar un tipo de int a una variable string
+//myFirstVariable = 1
+
+var mySecondVariable = "Hellow Hackerman!"
+
+print(mySecondVariable)
+
+mySecondVariable = myFirstVariable
+
+print(mySecondVariable)
+
+myFirstVariable = "Subscribete"
+
+print(mySecondVariable)
+
+//Constantes
+
+let myFirstConstant = "¿Te ha Gustado el tutorial?"
+
+print(myFirstConstant)
+
+//Una Constante no puede modificar su valor
+//myFirstConstant = "Si te ha gustado, dale a Like"
+
+let mySecondConstant = myFirstVariable
+
+print(mySecondConstant)
